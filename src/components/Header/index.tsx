a0783814c56791835578be1eafe7207bd2208d63
Ricardo Granvilla <rgranvilla@gmail.com>
2021-06-05 14:21:05 -0300

    posts implemation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { SignInButton } from "../SignInButton";
 
+import { ActiveLink } from "../ActiveLink";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -8,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
